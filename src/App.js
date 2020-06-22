d6bb70f2ae6007a4984a9e5cec0a24f24398b797
Brian Norman <briankn8@gmail.com>
2020-06-21 21:00:49 -0600

    Cobresun logo

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1>Cobresun</h1>
+    <div style={{flex: "1"}}>
+      <img style={{paddingTop: "10%", display: "block", width: "275px", height: "275px", margin: "auto"}} src="/logo.png" alt="Cobresun Logo"></img>
       <a href="/fadeaway-mac.zip" download>Fadeaway Mac Download</a>
       <br></br>
       <a href="/fadeaway-win.zip" download>Fadeaway Windows Download</a>
