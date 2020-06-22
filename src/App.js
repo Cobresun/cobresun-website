48fae96b42e4066531a353d3e30ceaad3aeafcbc
Brian Norman <briankn8@gmail.com>
2020-06-21 20:34:44 -0600

    Windows downloads

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div>
       <h1>Cobresun</h1>
       <a href="/fadeaway-mac.zip" download>Fadeaway Mac Download</a>
+      <br></br>
+      <a href="/fadeaway-win.zip" download>Fadeaway Windows Download</a>
     </div>
   )
 }
