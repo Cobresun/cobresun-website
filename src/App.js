64fa61d1d97c85880ac29c3f1e56ce8370d11a29
Brian Norman <briankn8@gmail.com>
2020-06-21 22:22:22 -0600

    React bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import './App.css'
+import Image from 'react-bootstrap/Image'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
 
 function App() {
   return (
-    <div style={{flex: "1"}}>
-      <img style={{paddingTop: "10%", display: "block", width: "275px", height: "275px", margin: "auto"}} src="/logo.png" alt="Cobresun Logo"></img>
-      <a href="/fadeaway-mac.zip" download>Fadeaway Mac Download</a>
-      <br></br>
-      <a href="/fadeaway-win.zip" download>Fadeaway Windows Download</a>
+    <div>
+      <Navbar expand="lg">
+        <Navbar.Brand href="#home">
+          <Image style={{width: "56px", height: "56px"}} src="/logo.png" alt="Cobresun Logo"/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#about">About Us</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <h1>About Us</h1>
+      <p>We might actually be the coolest devs...</p>
+
+      <div id="projects">
+        <h1>Projects</h1>
+        <a href="/fadeaway-mac.zip" download>Fadeaway Mac Download</a>
+        <br></br>
+        <a href="/fadeaway-win.zip" download>Fadeaway Windows Download</a>
+      </div>
+      
     </div>
   )
 }
