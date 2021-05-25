3130a73150390f8a0a45454f4b0069346f8ab028
Brian Norman <briankn8@gmail.com>
2021-05-25 11:05:02 -0600

    Add the naming moment image

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React from 'react'
 import './App.css'
-import Image from 'react-bootstrap/Image'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-
 
 function App() {
   return (
     <div>
-      <Navbar expand="lg">
-        <Navbar.Brand href="#home">
-          <Image style={{width: "56px", height: "56px"}} src="/logo.png" alt="Cobresun Logo"/>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#about">About Us</Nav.Link>
-          <Nav.Link href="#projects">Projects</Nav.Link>
-        </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-
       <div id="about">
-        <h1>About Us</h1>
+        <h1>Cobresun</h1>
         <p><b>🎂 Birthday: </b>June 24, 2014 (when the name first came into existence)</p>
         <p><b>🎯 Mission: </b>Make a video game 🎮</p>
         <b>👨‍👨‍👦‍👦 Members:</b>
@@ -33,12 +15,12 @@ function App() {
           <li><a rel="noopener noreferrer" target="_blank" href="https://wesley-tang.github.io">Wesley Tang</a></li>
           <li><a rel="noopener noreferrer" target="_blank" href="https://sunnynagam.github.io">Sunny Nagam</a></li>
         </ul>
+
+        <img src="naming-moment.png" alt="The moment Cobresun got its name."/>
       </div>
       
-      <div id="projects">
-        <h1>Projects</h1>
-        <p>Our projects on <a rel="noopener noreferrer" target="_blank" href="https://github.com/Cobresun">GitHub</a> and our apps on the <a rel="noopener noreferrer" target="_blank" href="https://play.google.com/store/apps/developer?id=Cobresun">Google Play Store</a>!</p>
-      </div>
+      <h1>Projects</h1>
+      <p>Our projects on <a rel="noopener noreferrer" target="_blank" href="https://github.com/Cobresun">GitHub</a> and our apps on the <a rel="noopener noreferrer" target="_blank" href="https://play.google.com/store/apps/developer?id=Cobresun">Google Play Store</a>!</p>
       
     </div>
   )
